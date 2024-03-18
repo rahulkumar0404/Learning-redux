131966b572d686cc98572727e543ee4660ccb0be
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-19 03:15:47 +0530

    added the show notification functionality

--- src/components/Shop/ProductItem.jsx
+++ src/components/Shop/ProductItem.jsx
@@ -1,20 +1,55 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Card from '../UI/Card.jsx';
 import classes from './ProductItem.module.css';
 import { cartActions } from '../../store/cart-slice.js';
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const disPatch = useDispatch();
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    disPatch(
-      cartActions.addItemToCart({
-        title,
-        id,
-        price,
-      })
-    );
+    const newTotalQuantity = cart.totalQuantity + 1;
+
+    const updatedItems = cart.items.slice();
+
+    const existingItem = updatedItems.find((item) => item.id === id);
+
+    if (existingItem) {
+      const updatedItem = { ...existingItem };
+
+      updatedItem.quantity++;
+
+      updatedItem.totalPrice = updatedItem.totalPrice + price;
+
+      const existingItemIndex = updatedItems.findIndex(
+        (item) => item.id === id
+      );
+
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
+    }
+
+    const newCart = {
+      totalQuantity: newTotalQuantity,
+      items: updatedItems,
+    };
+
+    disPatch(cartActions.replaceCart(newCart));
+    // disPatch(
+    //   cartActions.addItemToCart({
+    //     title,
+    //     id,
+    //     price,
+    //   })
+    // );
   };
   return (
     <li className={classes.item}>
