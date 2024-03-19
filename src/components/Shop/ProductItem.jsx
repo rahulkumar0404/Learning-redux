144c2cf18e55e7ea7691712c595848450bb2832a
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-19 19:49:46 +0530

    uncommented the previous line

--- src/components/Shop/ProductItem.jsx
+++ src/components/Shop/ProductItem.jsx
@@ -1,55 +1,55 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Card from '../UI/Card.jsx';
 import classes from './ProductItem.module.css';
 import { cartActions } from '../../store/cart-slice.js';
 
 const ProductItem = (props) => {
-  const cart = useSelector((state) => state.cart);
+  // const cart = useSelector((state) => state.cart);
   const disPatch = useDispatch();
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    const newTotalQuantity = cart.totalQuantity + 1;
+    // const newTotalQuantity = cart.totalQuantity + 1;
 
-    const updatedItems = cart.items.slice();
+    // const updatedItems = cart.items.slice();
 
-    const existingItem = updatedItems.find((item) => item.id === id);
+    // const existingItem = updatedItems.find((item) => item.id === id);
 
-    if (existingItem) {
-      const updatedItem = { ...existingItem };
+    // if (existingItem) {
+    //   const updatedItem = { ...existingItem };
 
-      updatedItem.quantity++;
+    //   updatedItem.quantity++;
 
-      updatedItem.totalPrice = updatedItem.totalPrice + price;
+    //   updatedItem.totalPrice = updatedItem.totalPrice + price;
 
-      const existingItemIndex = updatedItems.findIndex(
-        (item) => item.id === id
-      );
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
 
-      updatedItems[existingItemIndex] = updatedItem;
-    } else {
-      updatedItems.push({
-        id: id,
-        price: price,
-        quantity: 1,
-        totalPrice: price,
-        name: title,
-      });
-    }
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
 
-    const newCart = {
-      totalQuantity: newTotalQuantity,
-      items: updatedItems,
-    };
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
 
-    disPatch(cartActions.replaceCart(newCart));
-    // disPatch(
-    //   cartActions.addItemToCart({
-    //     title,
-    //     id,
-    //     price,
-    //   })
-    // );
+    // disPatch(cartActions.replaceCart(newCart));
+    disPatch(
+      cartActions.addItemToCart({
+        title,
+        id,
+        price,
+      })
+    );
   };
   return (
     <li className={classes.item}>
